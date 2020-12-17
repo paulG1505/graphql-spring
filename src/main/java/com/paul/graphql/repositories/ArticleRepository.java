c105c85f45d9d277d14d38a8c55d82fe6fc01df6
Paul Guaman <guaman.bryan.paul@gmail.com>
2020-12-17 08:31:53 -0500

    person

--- src/main/java/com/paul/graphql/repositories/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.paul.graphql.repositories;
-
-import com.paul.graphql.documents.Article;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ArticleRepository extends MongoRepository<Article, String> {
-}
